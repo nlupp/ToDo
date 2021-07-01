9ee967edf19c8133fa9853e51545ec0bd1c13cc8
nlupp <nlupp@brq.com>
2021-07-01 17:15:14 -0300

    nova tentariva do formulario, sem o problema de ter encontrado o JSON e ClassList

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
 $(document).ready(function () {
-    alert("eu to funcionando")
+    $("#inserirNota").click(inserirNota);
 });
+
+function inserirNota() {
+    var textoNota = $("#escreverNota").val();
+
+    var div = $("<div>");
+    var nota = $("<textarea>")
+    $(nota).html(textoNota);
+    $(nota).appendTo(div);
+   
+    var remover = $("<button>");
+    $(remover).html('Remover');
+    $(remover).on("click",function(){
+        div.remove();
+    });
+    $(remover).appendTo(div);
+
+    /
+    var editar =  $("<button>");
+    $(editar).html('editar');
+    $(editar).on("click",function(){
+       
+    });
+
+    $(div).appendTo('.container');
+    // dentro da div 
+}
+
+
+/*
+function remover(){
+
+};
+
+function editar() {
+
+};
+*/
---
+++ to_do.md
@@ -0,0 +1,20 @@
+# ToDo
+
+### Requisitos 1
+- Criar repositório no GitHub pessoal.
+- Realizar commits diários.
+
+### Requisitos 2
+- Botão para inserir nova nota.
+- Deve ser possível alterar o conteúdo das notas.
+- Deve ser possível excluir a nota selecionada.
+
+### Dicas
+- Utilizar os conceitos apresentados no workshop de JS. 
+
+**Css - Não se preocupar. Usem a critiavidade.**
+
+**Css - Indicações Pessoais.**
+- Usar for ou outro tipo de loop.
+- array com os li?
+- Tentar fazer algo parecido com uma tabela dinamica
