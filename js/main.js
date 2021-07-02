67107496e0614de4edac3c7b2dda12eeeab92fb7
nlupp <nlupp@brq.com>
2021-07-02 12:55:49 -0300

    lista funcional

--- js/main.js
+++ js/main.js
@@ -5,34 +5,46 @@ $(document).ready(function () {
 function inserirNota() {
     var textoNota = $("#escreverNota").val();
 
-    var div = $("<div>").addClass("");
+    var div = $("<div>").addClass("divconteudo");
     var nota = $("<textarea>")
+    $(nota).attr({
+        id: "tArea", class: "caixaDtexto",
+        rows: "2", cols: "50", maxlength: "100",
+        
+    });
     $(nota).html(textoNota);
-    
+
     $(nota).appendTo(div);
 
-    var editar = $("<button>").addClass("edicao");
+    var editar = $("<button>");
+    $(editar).attr({ id: "edButton", class: "edicao" });
     $(editar).html('editar');
     $(editar).on("click", function () {
-        
+        if ($(nota).prop('readonly') == false) {
+            $(nota).prop('readonly', true );
+        } else {
+            $(nota).prop('readonly', false);
+        }
+
     });
     $(editar).appendTo(div);
 
-    var remover = $("<button>").addClass("remocao");
+    var remover = $("<button>")
+    $(remover).attr({ id: "reButton", class: "remocao" });
     $(remover).html('Remover');
     $(remover).on("click", function () {
         div.remove();
     });
     $(remover).appendTo(div);
 
-    
+
     $(div).appendTo('.container');
 }
 
 /*
-if ($(nota).prop("disabled", false)) {
+if ($(nota).prop("readonly", false)) {
 } else {
-    $(nota).prop("disabled", true)
+    $(nota).prop("readonly, true)
 }
 
 /*
